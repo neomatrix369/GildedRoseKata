6b2a11b6b90371a3fd0258232bcbddc0502e0d5c
Mani Sarkar <sadhak001@gmail.com>
2015-09-25 16:10:41 +0100

    Refactored GildedRoseBySpecShould test into parameterised tests

--- src/test/java/com/gildedrose/GildedRoseBySpecShould.java
+++ src/test/java/com/gildedrose/GildedRoseBySpecShould.java
@@ -1,159 +1,115 @@
 package com.gildedrose;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static com.gildedrose.GildedRose.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@RunWith(Parameterized.class)
 public class GildedRoseBySpecShould {
 
-    @Test public void
-    increase_the_quality_of_Aged_Brie_by_1_as_item_has_not_passed_SellIn_date() {
-        GildedRose app = prepareGildedRoseWithItems(new Item[] {
-                new Item(AGED_BRIE, sellIn(1), qualityOf(1))
-        });
+    private String testCaseDescription;
+    private final String itemName;
+    private final int actualSellIn;
+    private final int actualQuality;
+    private final int expectedQuality;
 
-        app.updateQuality();
+    @Parameterized.Parameters(name = "{0} with sell in {1} and quality of {2}, changes to quality of {3}.")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(
+                new Object[][]{
+                        {
+                                "increase the quality of Aged Brie by 1 as item has not passed SellIn date",
+                                AGED_BRIE, sellIn(1), qualityOf(1), qualityOf(2)
+                        },
+                        {
+                                "increase the quality of Aged Brie by 2 as item has passed SellIn date",
+                                AGED_BRIE, sellIn(0), qualityOf(2), qualityOf(4)
+                        },
+                        {
+                                "never change the quality of item Sufluras as time passes",
+                                SULFURAS_HAND_OF_RAGNAROS, sellIn(0), qualityOf(2), qualityOf(2),
+                        },
+                        {
+                                "never change the quality of any item past 50",
+                                AGED_BRIE, sellIn(0), qualityOf(50), qualityOf(50),
+                        },
+                        {
+                                "increase the quality of Backstage Passes by 2 when sellIn for it is 10 days",
+                                BACKSTAGE_PASSES, sellIn(10), qualityOf(25), qualityOf(27)
+                        },
+                        {
+                                "increase the quality of Backstage Passes by 2 when sellIn for it is 8 days",
+                                BACKSTAGE_PASSES, sellIn(8), qualityOf(20), qualityOf(22)
+                        },
+                        {
+                                "increase the quality of Backstage Passes by 3 when sellIn for it is 5 days",
+                                BACKSTAGE_PASSES, sellIn(5), qualityOf(22), qualityOf(25)
+                        },
+                        {
+                                "increase the quality of Backstage Passes by 3 when sellIn for it is 3 days",
+                                BACKSTAGE_PASSES, sellIn(3), qualityOf(27), qualityOf(30)
+                        },
+                        {
+                                "decrease the quality of Backstage Passes to 0 when sellIn for it is 0 days",
+                                BACKSTAGE_PASSES, sellIn(0), qualityOf(27), qualityOf(0)
+                        },
+                        {
+                                "decrease the quality of any other item by 1 when quality is greater than 0",
+                                ANY_OTHER_ITEM, sellIn(1), qualityOf(25), qualityOf(24)
+                        },
+                        {
+                                "decrease the quality of any other item by 2 when quality is greater than 0 and sellIn less than 1",
+                                ANY_OTHER_ITEM, sellIn(-1), qualityOf(23), qualityOf(21)
+                        },
+                        {
+                                "decrease the quality of Conjured item by 2 when quality is greater than 0 and sellIn less than 1",
+                                CONJURED, sellIn(-1), qualityOf(19), qualityOf(17)
+                        }
 
-        assertThat(app.items[0].quality, is(2));
+                }
+        );
     }
 
-    @Test public void
-    increase_the_quality_of_Aged_Brie_by_2_as_item_has_passed_SellIn_date() {
-        GildedRose app = prepareGildedRoseWithItems(new Item[] {
-                new Item(AGED_BRIE, sellIn(0), qualityOf(2))
-        });
-
-        app.updateQuality();
-
-        assertThat(app.items[0].quality, is(4));
+    public GildedRoseBySpecShould(String testCaseDescription,
+                                  String itemName,
+                                  int actualSellIn,
+                                  int actualQuality,
+                                  int expectedQuality) {
+        this.testCaseDescription = testCaseDescription;
+        this.itemName = itemName;
+        this.actualSellIn = actualSellIn;
+        this.actualQuality = actualQuality;
+        this.expectedQuality = expectedQuality;
     }
 
-    @Test public void
-    never_change_the_quality_of_item_Sufluras_as_time_passes() {
-        GildedRose app = prepareGildedRoseWithItems(new Item[] {
-                new Item(SULFURAS_HAND_OF_RAGNAROS, sellIn(0), qualityOf(2))
+    @Test
+    public void
+    change_the_quality_of_an_item_under_the_given_conditions() {
+        GildedRose app = prepareGildedRoseWithItems(new Item[]{
+                new Item(itemName, actualSellIn, actualQuality)
         });
 
         app.updateQuality();
 
-        assertThat(app.items[0].quality, is(2));
-    }
-
-    @Test public void
-    never_change_the_quality_of_any_item_past_50() {
-        GildedRose app = prepareGildedRoseWithItems(new Item[] {
-                new Item(AGED_BRIE, sellIn(0), qualityOf(50))
-        });
-
-        app.updateQuality();
-
-        assertThat(app.items[0].quality, is(50));
-    }
-
-    @Test public void
-    increase_the_quality_of_backstage_passes_by_2_when_sellIn_for_it_is_10_days() {
-        GildedRose app = prepareGildedRoseWithItems(new Item[] {
-                new Item(BACKSTAGE_PASSES, sellIn(10), qualityOf(25))
-        });
-
-        app.updateQuality();
-
-        assertThat(app.items[0].quality, is(27));
-    }
-
-
-    @Test public void
-    increase_the_quality_of_backstage_passes_by_2_when_sellIn_for_it_is_8_days() {
-        GildedRose app = prepareGildedRoseWithItems(new Item[] {
-                new Item(BACKSTAGE_PASSES, sellIn(8), qualityOf(20))
-        });
-
-        app.updateQuality();
-
-        assertThat(app.items[0].quality, is(22));
-    }
-
-    @Test public void
-    increase_the_quality_of_backstage_passes_by_3_when_sellIn_for_it_is_5_days() {
-        GildedRose app = prepareGildedRoseWithItems(new Item[] {
-                new Item(BACKSTAGE_PASSES, sellIn(5), qualityOf(22))
-        });
-
-        app.updateQuality();
-
-        assertThat(app.items[0].quality, is(25));
-    }
-
-
-    @Test public void
-    increase_the_quality_of_backstage_passes_by_3_when_sellIn_for_it_is_3_days() {
-        GildedRose app = prepareGildedRoseWithItems(new Item[] {
-                new Item(BACKSTAGE_PASSES, sellIn(3), qualityOf(27))
-        });
-
-        app.updateQuality();
-
-        assertThat(app.items[0].quality, is(30));
-    }
-
-
-    @Test public void
-    decrease_the_quality_of_backstage_passes_to_0_when_sellIn_for_it_is_0_days() {
-        GildedRose app = prepareGildedRoseWithItems(new Item[] {
-                new Item(BACKSTAGE_PASSES, sellIn(0), qualityOf(27))
-        });
-
-        app.updateQuality();
-
-        assertThat(app.items[0].quality, is(0));
-    }
-
-    @Test public void
-    decrease_the_quality_of_any_other_item_by_1_when_quality_is_greater_than_0() {
-        GildedRose app = prepareGildedRoseWithItems(new Item[] {
-                new Item(ANY_OTHER_ITEM, sellIn(1), qualityOf(25))
-        });
-
-        app.updateQuality();
-
-        assertThat(app.items[0].quality, is(24));
-    }
-
-    @Test public void
-    decrease_the_quality_of_any_other_item_by_2_when_quality_is_greater_than_0_and_sellIn_less_than_1() {
-        GildedRose app = prepareGildedRoseWithItems(new Item[] {
-                new Item(ANY_OTHER_ITEM, sellIn(-1), qualityOf(23))
-        });
-
-        app.updateQuality();
-
-        assertThat(app.items[0].quality, is(21));
-    }
-
-
-    @Test public void
-    decrease_the_quality_of_conjured_item_by_2_when_quality_is_greater_than_0_and_sellIn_less_than_1() {
-        GildedRose app = prepareGildedRoseWithItems(new Item[] {
-                new Item(CONJURED, sellIn(-1), qualityOf(19))
-        });
-
-        app.updateQuality();
-
-        assertThat(app.items[0].quality, is(17));
+        assertThat(app.items[0].quality, is(expectedQuality));
     }
 
     private GildedRose prepareGildedRoseWithItems(Item[] items) {
         return new GildedRose(items);
     }
 
-    private int sellIn(int value) {
+    private static int sellIn(int value) {
         return value;
     }
 
-    private int qualityOf(int value) {
+    private static int qualityOf(int value) {
         return value;
     }
-
 }
