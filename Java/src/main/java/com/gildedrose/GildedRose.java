cf31900a798bcb70d3cadf7f255e04a8ea58fce8
neomatrix369 <sadhak001@gmail.com>
2015-09-28 00:39:53 +0100

    A number of refactorings to the implementation, tests are still green

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -15,53 +15,70 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals(AGED_BRIE)
-                    && !items[i].name.equals(BACK_STAGE_PASSES)) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals(BACK_STAGE_PASSES)) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+                    && !items[i].name.equals(BACK_STAGE_PASSES)
+                && !items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)
+                        && (items[i].quality > 0)) {
+                    decreaseQuality(items[i]);
             }
 
-            if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if ((items[i].name.equals(AGED_BRIE)
+                    || items[i].name.equals(BACK_STAGE_PASSES)) &&
+                       (items[i].quality < 50)) {
+                    increaseQuality(items[i]);
+                    increaseQualityOfBackStagePasses(items[i], 11, 50);
+                    increaseQualityOfBackStagePasses(items[i], 6, 50);
             }
+
+            decreaseSellInForAllItemsOtherThanSulfurus(items[i]);
 
             if (items[i].sellIn < 0) {
+                if (items[i].name.equals(AGED_BRIE)) {
+                    increaseQuality(items[i], 50);
+                }
+
                 if (!items[i].name.equals(AGED_BRIE)) {
                     if (!items[i].name.equals(BACK_STAGE_PASSES)) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                                items[i].quality = items[i].quality - 1;
-                            }
+                        if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)
+                                && (items[i].quality > 0)) {
+                            decreaseQuality(items[i]);
                         }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
                     }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+
+                    if (items[i].name.equals(BACK_STAGE_PASSES)) {
+                        items[i].quality = 0;
                     }
                 }
             }
         }
+    }
+
+    private void decreaseSellInForAllItemsOtherThanSulfurus(Item item) {
+        if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+            item.sellIn = item.sellIn - 1;
+        }
+    }
+
+    private void increaseQualityOfBackStagePasses(Item item,
+                                                  int minimumSellIn,
+                                                  int maximumQuality) {
+        if (item.name.equals(BACK_STAGE_PASSES)
+            && (item.sellIn < minimumSellIn)
+                && (item.quality < maximumQuality)) {
+                increaseQuality(item);
+        }
+    }
+
+    private void increaseQuality(Item item) {
+        item.quality = item.quality + 1;
+    }
+
+    private void increaseQuality(Item item, int maximumQuality) {
+        if (item.quality < maximumQuality) {
+            increaseQuality(item);
+        }
+    }
+
+    private void decreaseQuality(Item item) {
+        item.quality = item.quality - 1;
     }
 }
