71ac2cba01eadfc604af3a24ebf5d8322284b634
neomatrix369 <sadhak001@gmail.com>
2015-09-28 00:16:20 +0100

    Created java folder structure. Added .gitignore, added pom.xml. Wrote tests from specification, with full code coverage.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,11 @@
 package com.gildedrose;
 
 class GildedRose {
+    public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String ANY_ITEM = "Any";
+    public static final String BACK_STAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -9,10 +14,10 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!items[i].name.equals(AGED_BRIE)
+                    && !items[i].name.equals(BACK_STAGE_PASSES)) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
@@ -20,7 +25,7 @@ class GildedRose {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (items[i].name.equals(BACK_STAGE_PASSES)) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
@@ -36,15 +41,15 @@ class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (!items[i].name.equals(AGED_BRIE)) {
+                    if (!items[i].name.equals(BACK_STAGE_PASSES)) {
                         if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
---
+++ Java/src/main/java/com/gildedrose/GildedRoseBySpecShould.java
@@ -0,0 +1,114 @@
+package com.gildedrose;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static com.gildedrose.GildedRose.AGED_BRIE;
+import static com.gildedrose.GildedRose.ANY_ITEM;
+import static com.gildedrose.GildedRose.BACK_STAGE_PASSES;
+import static com.gildedrose.GildedRose.SULFURAS_HAND_OF_RAGNAROS;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(Parameterized.class)
+public class GildedRoseBySpecShould {
+
+    private final String useCaseDescription;
+    private final String itemName;
+    private final int actualSellIn;
+    private final int actualQuality;
+    private final int expectedQuality;
+
+    @Parameterized.Parameters(name = "{0}: Quality of item {1} with a sell in days of {2}, and quality of {3}, gets quality of {4}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(
+                new Object[][]{
+                        {"Decrease quality by 2 if item has passed its sell in date",
+                                ANY_ITEM, sellIn(-1), qualityOf(2), qualityOf(0)},
+                        {"The Quality of an item is never negative (past sell in date)",
+                                ANY_ITEM, sellIn(-1), qualityOf(1), qualityOf(0)},
+                        {"The Quality of an item is never negative (past sell in date)",
+                                ANY_ITEM, sellIn(0), qualityOf(1), qualityOf(0)},
+                        {"The Quality of an item is never negative (past sell in date)",
+                                ANY_ITEM, sellIn(-1), qualityOf(0), qualityOf(0)},
+
+                        {"The Quality of Aged Brie can never more than 50 (near sell in date of 1)",
+                                AGED_BRIE, sellIn(1), qualityOf(50), qualityOf(50)},
+                        {"The Quality of Aged Brie increases by 1 when before sell in date",
+                                AGED_BRIE, sellIn(1), qualityOf(48), qualityOf(49)},
+                        {"The Quality of Aged Brie increases by 2 when past sell in date",
+                                AGED_BRIE, sellIn(0), qualityOf(48), qualityOf(50)},
+                        {"The Quality of Aged Brie can never more than 50 (near sell in date of 0)",
+                                AGED_BRIE, sellIn(0), qualityOf(50), qualityOf(50)},
+                        {"The Quality of Aged Brie increases by 2 but can never more than 50 (past sell in date)",
+                                AGED_BRIE, sellIn(-1), qualityOf(50), qualityOf(50)},
+                        {"The Quality of Aged Brie increases by 2 even if its past sell in date",
+                                AGED_BRIE, sellIn(-1), qualityOf(48), qualityOf(50)},
+
+                        {"The Quality of Sulfurus never changes (stays at 80) (past sell in days)",
+                                SULFURAS_HAND_OF_RAGNAROS, sellIn(0), qualityOf(80), qualityOf(80)},
+                        {"The Quality of Sulfurus never changes (stays at 80) (past sell in days)",
+                                SULFURAS_HAND_OF_RAGNAROS, sellIn(-1), qualityOf(80), qualityOf(80)},
+                        {"The Quality of Sulfurus never changes (stays at 80) (within sell in days)",
+                                SULFURAS_HAND_OF_RAGNAROS, sellIn(1), qualityOf(80), qualityOf(80)},
+
+                        {"The Quality of BackStage Passes increases by 1 for 11 or more days of sell in days",
+                                BACK_STAGE_PASSES, sellIn(11), qualityOf(10), qualityOf(11)},
+                        {"The Quality of BackStage Passes increases by 2 when within 10 or less days of sell in days (10 days)",
+                                BACK_STAGE_PASSES, sellIn(10), qualityOf(10), qualityOf(12)},
+                        {"The Quality of BackStage Passes increases by 2 when within 10 or less days of sell in days (6 days)",
+                                BACK_STAGE_PASSES, sellIn(6), qualityOf(12), qualityOf(14)},
+                        {"The Quality of BackStage Passes increases by 3 when within 5 or less days of sell in days (5 days)",
+                                BACK_STAGE_PASSES, sellIn(5), qualityOf(12), qualityOf(15)},
+                        {"The Quality of BackStage Passes increases by 3 when within 1 or less days of sell in days (1 day)",
+                                BACK_STAGE_PASSES, sellIn(1), qualityOf(12), qualityOf(15)},
+                        {"The Quality of BackStage Passes drops to 0 after the concert (finished by a day)",
+                                BACK_STAGE_PASSES, sellIn(0), qualityOf(10), qualityOf(0)},
+                        {"The Quality of BackStage Passes drops to 0 after the concert (finished by 2 days)",
+                                BACK_STAGE_PASSES, sellIn(-1), qualityOf(10), qualityOf(0)},
+
+                }
+        );
+    }
+
+    public GildedRoseBySpecShould(
+            String useCaseDescription,
+            String itemName,
+            int actualSellIn,
+            int actualQuality,
+            int expectedQuality) {
+        this.useCaseDescription = useCaseDescription;
+        this.itemName = itemName;
+        this.actualSellIn = actualSellIn;
+        this.actualQuality = actualQuality;
+        this.expectedQuality = expectedQuality;
+    }
+
+    @Test
+    public void
+    change_quality_item_with_respective_sellin_date_and_starting_quality() {
+        GildedRose app = prepareGildedRose();
+
+        app.updateQuality();
+
+        assertThat(useCaseDescription, app.items[0].quality, is(equalTo(expectedQuality)));
+    }
+
+    private GildedRose prepareGildedRose() {
+        Item[] items = new Item[]{new Item(itemName, actualSellIn, actualQuality)};
+        return new GildedRose(items);
+    }
+
+    private static int qualityOf(int value) {
+        return value;
+    }
+
+    private static int sellIn(int value) {
+        return value;
+    }
+}
