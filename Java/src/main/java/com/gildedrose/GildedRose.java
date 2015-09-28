8918dc57d5155dce0618ec2601c8f0e7ee5f29ed
neomatrix369 <sadhak001@gmail.com>
2015-09-28 01:04:21 +0100

    Even more refactorings, inlined many functions, groups are beginning to appear

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -27,15 +27,29 @@ class GildedRose {
                 increaseQuality(items[i]);
             }
 
-            increaseQualityOfBackStagePasses(items[i], 11, 50);
-            increaseQualityOfBackStagePasses(items[i], 6, 50);
-            increaseQualityOfBackStagePasses(items[i], 50);
+            if (items[i].name.equals(BACK_STAGE_PASSES)
+                    && (items[i].sellIn < 11)
+                    && (items[i].quality < 50)) {
+                increaseQuality(items[i]);
+            }
+            if (items[i].name.equals(BACK_STAGE_PASSES)
+                    && (items[i].sellIn < 6)
+                    && (items[i].quality < 50)) {
+                increaseQuality(items[i]);
+            }
 
-            decreaseSellInForAllItemsExceptSulfurus(items[i]);
+            if (items[i].name.equals(BACK_STAGE_PASSES)) {
+                increaseQuality(items[i]);
+            }
+
+            if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+                decreaseSellIn(i);
+            }
 
             if (items[i].name.equals(AGED_BRIE)
-                    && (items[i].sellIn < 0)) {
-                increaseQuality(items[i], 50);
+                    && (items[i].sellIn < 0)
+                    && items[i].quality < 50) {
+                increaseQuality(items[i]);
             }
 
             if (!items[i].name.equals(AGED_BRIE)
@@ -46,47 +60,23 @@ class GildedRose {
                 decreaseQuality(items[i]);
             }
 
-            reduceQualityForBackStagePassToZeroIfPastSellinDate(items[i]);
+            if (items[i].name.equals(BACK_STAGE_PASSES)
+                    && (items[i].sellIn < 0)) {
+                setQualityToZero(i);
+            }
         }
     }
 
-    private void reduceQualityForBackStagePassToZeroIfPastSellinDate(Item item) {
-        if (item.name.equals(BACK_STAGE_PASSES)
-                && (item.sellIn < 0)) {
-            item.quality = 0;
-        }
+    private void setQualityToZero(int i) {
+        items[i].quality = 0;
     }
 
-    private void increaseQualityOfBackStagePasses(Item item, int maximumQuality) {
-        if (item.name.equals(BACK_STAGE_PASSES)) {
-            increaseQuality(item);
-        }
-    }
-
-    private void increaseQualityOfBackStagePasses(Item item,
-                                                  int minimumSellIn,
-                                                  int maximumQuality) {
-        if (item.name.equals(BACK_STAGE_PASSES)
-                && (item.sellIn < minimumSellIn)
-                && (item.quality < maximumQuality)) {
-            increaseQuality(item);
-        }
-    }
-
-    private void decreaseSellInForAllItemsExceptSulfurus(Item item) {
-        if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-            item.sellIn = item.sellIn - 1;
-        }
+    private void decreaseSellIn(int i) {
+        items[i].sellIn = items[i].sellIn - 1;
     }
 
     private void increaseQuality(Item item) {
         item.quality = item.quality + 1;
-    }
-
-    private void increaseQuality(Item item, int maximumQuality) {
-        if (item.quality < maximumQuality) {
-            increaseQuality(item);
-        }
     }
 
     private void decreaseQuality(Item item) {
