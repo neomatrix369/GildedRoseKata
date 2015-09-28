94d884906d32c69642c785b629de19974a68978a
Mani Sarkar <sadhak001@gmail.com>
2015-09-28 11:44:53 +0100

    Resurrected Java project for Gilded rose

--- js/spec/gilded_rose_spec.js
+++
@@ -1,9 +0,0 @@
-describe("Gilded Rose", function() {
-
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
-    update_quality();
-    expect(items[0].name).toEqual("fixme");
-  });
-
-});
