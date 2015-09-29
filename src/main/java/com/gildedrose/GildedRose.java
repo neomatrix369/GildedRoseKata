e05ee99521bfdb721999b8889b0ae7fb4b9102ee
Mani Sarkar <sadhak001@gmail.com>
2015-09-29 14:10:56 +0100

    Inlined method call to do with Aged Brie, removed nested ifs

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -10,8 +10,9 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (items[i].name.equals("Aged Brie")) {
-                increaseQualityIfQualityIsLessThan_50(items[i]);
+            if (items[i].name.equals("Aged Brie")
+                    && items[i].quality < 50) {
+                items[i].quality = items[i].quality + 1;
             }
 
             if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
@@ -56,21 +57,6 @@ class GildedRose {
     private void decreaseSellInIfItemIsOtherThanSulfuras(Item item) {
         if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
             item.sellIn = item.sellIn - 1;
-        }
-    }
-
-    private void decreaseQualityIfQualityIsGreaterThan_0_ForItemsOtherThanAgedBriedBackstagePassesAndSulfuras(Item item) {
-        if (!item.name.equals("Aged Brie")
-                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")
-                && !item.name.equals("Sulfuras, Hand of Ragnaros")
-                && item.quality > 0) {
-            item.quality = item.quality - 1;
-        }
-    }
-
-    private void setQualityTo_0_BackstagePasses(Item item) {
-        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            item.quality = 0;
         }
     }
 
