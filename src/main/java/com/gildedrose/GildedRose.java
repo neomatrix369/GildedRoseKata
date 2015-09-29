370c8f29c7ddb18e3dedccf9a3eaa03b8e8dd7b7
Mani Sarkar <sadhak001@gmail.com>
2015-09-29 14:02:18 +0100

    Inlined method call to do with Aged Brie, removed nested ifs

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -32,8 +32,10 @@ class GildedRose {
 
             decreaseSellInIfItemIsOtherThanSulfuras(items[i]);
 
-            if (items[i].name.equals("Aged Brie") && items[i].sellIn < 0) {
-                increaseQualityIfQualityIsLessThan_50(items[i]);
+            if (items[i].name.equals("Aged Brie")
+                    && items[i].sellIn < 0
+                    && items[i].quality < 50) {
+                items[i].quality = items[i].quality + 1;
             }
 
             if (items[i].sellIn < 0) {
@@ -44,6 +46,7 @@ class GildedRose {
                 decreaseQualityIfQualityIsGreaterThan_0_ForItemsOtherThanAgedBriedBackstagePassesAndSulfuras(items[i]);
             }
         }
+
     }
 
     private void decreaseSellInIfItemIsOtherThanSulfuras(Item item) {
