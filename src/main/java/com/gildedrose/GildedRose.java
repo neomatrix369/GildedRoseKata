0896dc84622428de418115a8f03c6e4e2d1039dc
Mani Sarkar <sadhak001@gmail.com>
2015-09-29 15:10:34 +0100

    Brought all the if states into sync with each other i.e. layout of the conditions and body

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -10,15 +10,15 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (items[i].name.equals("Aged Brie")
-                    && items[i].quality < 50) {
-                increaseQuality(items[i]);
-                decreaseSellInDays(items[i]);
-
-                if (items[i].sellIn < 0) {
+            if (items[i].name.equals("Aged Brie"))
+                if (items[i].quality < 50) {
                     increaseQuality(items[i]);
+                    decreaseSellInDays(items[i]);
+
+                    if (items[i].sellIn < 0) {
+                        increaseQuality(items[i]);
+                    }
                 }
-            }
 
             if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 increaseQuality(items[i]);
@@ -42,12 +42,14 @@ class GildedRose {
 
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")
-                    && !items[i].name.equals("Sulfuras, Hand of Ragnaros")
-                    && (items[i].quality > 0)) {
-                decreaseQuality(items[i]);
-                decreaseSellInDays(items[i]);
-                if (items[i].sellIn < 0) {
+                    && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                if (items[i].quality > 0) {
                     decreaseQuality(items[i]);
+                    decreaseSellInDays(items[i]);
+
+                    if (items[i].sellIn < 0) {
+                        decreaseQuality(items[i]);
+                    }
                 }
             }
 
