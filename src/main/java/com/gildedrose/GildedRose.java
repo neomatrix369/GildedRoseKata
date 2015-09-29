1c85e9803419ef024f85c00050c93c4a0319ebad
Mani Sarkar <sadhak001@gmail.com>
2015-09-29 10:48:51 +0100

    Refactored a few more blocks of code into individual methods, and renamed already refactored code.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -12,22 +12,18 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                decreaseQualityIfQualityIsGreaterThanZeroAndItemIsNotSulfuras(items[i]);
+                decreaseQualityIfQualityIsGreaterThan_0_AndItemIsNotSulfuras(items[i]);
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                            increaseQualityIfQualityIsLessThan_50(items[i]);
                         }
 
                         if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                            increaseQualityIfQualityIsLessThan_50(items[i]);
                         }
                     }
                 }
@@ -40,20 +36,24 @@ class GildedRose {
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        decreaseQualityIfQualityIsGreaterThanZeroAndItemIsNotSulfuras(items[i]);
+                        decreaseQualityIfQualityIsGreaterThan_0_AndItemIsNotSulfuras(items[i]);
                     } else {
                         items[i].quality = items[i].quality - items[i].quality;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+                    increaseQualityIfQualityIsLessThan_50(items[i]);
                 }
             }
         }
     }
 
-    private void decreaseQualityIfQualityIsGreaterThanZeroAndItemIsNotSulfuras(Item item) {
+    private void increaseQualityIfQualityIsLessThan_50(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+    }
+
+    private void decreaseQualityIfQualityIsGreaterThan_0_AndItemIsNotSulfuras(Item item) {
         if (item.quality > 0) {
             decreaseQualityIfItemIsNotSulfuras(item);
         }
