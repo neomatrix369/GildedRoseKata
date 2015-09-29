0b6f29ee795b99c23c83133934825527035b6b41
Mani Sarkar <sadhak001@gmail.com>
2015-09-29 13:46:32 +0100

    Combine nested if and delete unused conditions

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -10,9 +10,8 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (items[i].name.equals("Aged Brie")
-                    || items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                increaseQualityOfAgedBrieIfQualityIsLessThan_50(items[i]);
+            if (items[i].name.equals("Aged Brie")) {
+                increaseQualityIfQualityIsLessThan_50(items[i]);
             }
 
             if (items[i].name.equals("Aged Brie")
@@ -85,12 +84,6 @@ class GildedRose {
         if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             increaseQualityIfQualityIsLessThan_50_AndSellInIsLessThanMaxSellIn(item, 11);
             increaseQualityIfQualityIsLessThan_50_AndSellInIsLessThanMaxSellIn(item, 6);
-        }
-    }
-
-    private void decreaseQualityIfQualityIsGreaterThan_0_AndItemIsNotSulfurasOrBackStagePasses(Item item) {
-        if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            decreaseQualityIfQualityIsGreaterThan_0_AndItemIsNotSulfuras(item);
         }
     }
 
