f25de31ea45cdc4bd3073d49ad586c778360fb21
Mani Sarkar <sadhak001@gmail.com>
2015-10-08 11:33:28 +0100

    Refactored the dictionary in the factory to map to class literals of item types instead of instances of item types itself.

--- src/main/java/com/gildedrose/ItemUpdater.java
+++ src/main/java/com/gildedrose/ItemUpdater.java
@@ -11,17 +11,28 @@ public class ItemUpdater {
     public static final String CONJURED = "Conjured";
     public static final String ANY_OTHER_ITEM = "Any Other Item";
 
-    private static Map<String, UpdatableItem> itemNameToUpdatableItem = new HashMap<String, UpdatableItem>() {
+    private static Map<String, Class<? extends UpdatableItem>> itemNameToUpdatableItem = new HashMap<String, Class<? extends UpdatableItem>>() {
         {
-            put(AGED_BRIE, new AgedBrie());
-            put(BACKSTAGE_PASSES, new BackstagePasses());
-            put(SULFURAS_HAND_OF_RAGNAROS, new Sulfuras());
-            put(CONJURED, new Conjured());
+            put(AGED_BRIE, AgedBrie.class);
+            put(BACKSTAGE_PASSES, BackstagePasses.class);
+            put(SULFURAS_HAND_OF_RAGNAROS, Sulfuras.class);
+            put(CONJURED, Conjured.class);
         }
     };
 
     public static void update(Item item) {
-        UpdatableItem updatableItem = itemNameToUpdatableItem.getOrDefault(item.name, new DefaultItem());
+        UpdatableItem updatableItem = createInstanceFromClass(
+                itemNameToUpdatableItem.getOrDefault(item.name, DefaultItem.class)
+        );
         updatableItem.update(item);
+    }
+
+    private static UpdatableItem createInstanceFromClass(Class<? extends UpdatableItem> classOfUpdatableItem) {
+        try {
+            return classOfUpdatableItem.newInstance();
+        } catch (InstantiationException | IllegalAccessException ex) {
+            throw new IllegalArgumentException(String.format("Error with item of class %s, error message: %s.",
+                    classOfUpdatableItem.getSimpleName(), ex.getMessage()));
+        }
     }
 }
