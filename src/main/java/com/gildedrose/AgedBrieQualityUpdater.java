92dce4fae43af58c8f1f0d79629ff199f17a8e27
Mani Sarkar <sadhak001@gmail.com>
2015-10-12 13:32:10 +0100

    Removed unused constants in Aged Brie updater class

--- src/main/java/com/gildedrose/AgedBrieQualityUpdater.java
+++ src/main/java/com/gildedrose/AgedBrieQualityUpdater.java
@@ -2,9 +2,6 @@ package com.gildedrose;
 
 public class AgedBrieQualityUpdater implements QualityUpdater {
 
-    private static final int MAXIMUM_QUALITY = 50;
-    private static final int MINIMUM_SELL_IN = 0;
-
     @Override
     public void update(Item item) {
         if (item.quality < MAXIMUM_QUALITY) {
