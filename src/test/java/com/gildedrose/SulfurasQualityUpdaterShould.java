7eb59bd054b35b536fc1740c79fc9b120c96b650
Mani Sarkar <sadhak001@gmail.com>
2015-10-13 17:11:47 +0100

    Added new test class for SulfurusQualityUpdater, added a simple test to check the updater

---
+++ src/test/java/com/gildedrose/SulfurasQualityUpdaterShould.java
@@ -0,0 +1,24 @@
+package com.gildedrose;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SulfurasQualityUpdaterShould {
+
+    @Test public void
+    not_change_quality_of_the_item() {
+        int initialSellIn = 0;
+        int initialQuality = 80;
+        Item item = new Item(Constants.SULFURAS, initialSellIn, initialQuality);
+
+        SulfurasQualityUpdater sulfurasQualityUpdater = new SulfurasQualityUpdater(item);
+        sulfurasQualityUpdater.update();
+
+        int expectedQuality = 80;
+        assertThat(item.quality, is(equalTo(expectedQuality)));
+    }
+
+}
