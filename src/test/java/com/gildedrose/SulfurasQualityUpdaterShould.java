cb35368d2632fa5df13397d335cad168a1529dcc
Mani Sarkar <sadhak001@gmail.com>
2015-10-13 19:18:12 +0100

    Refactored SulfurasQualityUpdater test class, removed redundant code.

--- src/test/java/com/gildedrose/SulfurasQualityUpdaterShould.java
+++ src/test/java/com/gildedrose/SulfurasQualityUpdaterShould.java
@@ -3,6 +3,9 @@ package com.gildedrose;
 import org.junit.Test;
 
 import static com.gildedrose.Constants.SULFURAS;
+import static com.gildedrose.TestHelper.createItemFrom;
+import static com.gildedrose.TestHelper.qualityOf;
+import static com.gildedrose.TestHelper.sellInOf;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -11,15 +14,11 @@ public class SulfurasQualityUpdaterShould {
 
     @Test public void
     not_change_quality_of_the_item() {
-        int initialSellIn = 0;
-        int initialQuality = 80;
-        Item item = new Item(SULFURAS, initialSellIn, initialQuality);
+        Item item = createItemFrom(SULFURAS, sellInOf(0), qualityOf(80));
 
-        SulfurasQualityUpdater sulfurasQualityUpdater = new SulfurasQualityUpdater(item);
-        sulfurasQualityUpdater.update();
+        new SulfurasQualityUpdater(item).update();
 
-        int expectedQuality = 80;
-        assertThat(item.quality, is(equalTo(expectedQuality)));
+        assertThat(item.quality, is(equalTo(qualityOf(80))));
     }
 
 }
