2932df32beb891e2fe2608bb38d8e4af38437f06
Mani Sarkar <sadhak001@gmail.com>
2015-10-13 19:26:48 +0100

    Refactored SulfurasSellInUpdater test class, removed redundant code, replaced with static method calls to sellInOf() and qualityOf()

--- src/test/java/com/gildedrose/SulfurasSellInUpdaterShould.java
+++ src/test/java/com/gildedrose/SulfurasSellInUpdaterShould.java
@@ -3,6 +3,8 @@ package com.gildedrose;
 import org.junit.Test;
 
 import static com.gildedrose.Constants.SULFURAS;
+import static com.gildedrose.TestHelper.qualityOf;
+import static com.gildedrose.TestHelper.sellInOf;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -10,15 +12,11 @@ import static org.junit.Assert.assertThat;
 public class SulfurasSellInUpdaterShould {
     @Test public void
     not_change_the_sell_in_of_the_item() {
-        int initialQuality = 1;
-        int initialSellIn = 10;
-        Item item = new Item(SULFURAS, initialSellIn, initialQuality);
+        Item item = new Item(SULFURAS, sellInOf(10), qualityOf(1));
 
-        SulfurasSellInUpdater sulfurasSellInUpdater = new SulfurasSellInUpdater(item);
-        sulfurasSellInUpdater.update();
+        new SulfurasSellInUpdater(item).update();
 
-        int expectedSellIn = 10;
-        assertThat(item.sellIn, is(equalTo(expectedSellIn)));
+        assertThat(item.sellIn, is(equalTo(sellInOf(10))));
     }
 
 }
