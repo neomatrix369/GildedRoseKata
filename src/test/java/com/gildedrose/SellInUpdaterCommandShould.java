7337c1096a5afd141c3616c7c8111cda1cabae60
Mani Sarkar <sadhak001@gmail.com>
2015-10-13 19:40:26 +0100

    Statically imported constants in SellInUpdaterCommandShould.

--- src/test/java/com/gildedrose/SellInUpdaterCommandShould.java
+++ src/test/java/com/gildedrose/SellInUpdaterCommandShould.java
@@ -2,6 +2,9 @@ package com.gildedrose;
 
 import org.junit.Test;
 
+import static com.gildedrose.Constants.*;
+import static com.gildedrose.Constants.CONJURED;
+import static com.gildedrose.Constants.SULFURAS;
 import static com.gildedrose.TestHelper.createItemFrom;
 import static com.gildedrose.TestHelper.expectedSellInOf;
 import static com.gildedrose.TestHelper.qualityOf;
@@ -14,7 +17,7 @@ public class SellInUpdaterCommandShould {
 
     @Test public void
     use_the_SulfurasSellInUpdater_for_a_Sulfuras_item_and_item_sellIn_should_not_change() {
-        Item item = createItemFrom(Constants.SULFURAS, sellInOf(1), qualityOf(1));
+        Item item = createItemFrom(SULFURAS, sellInOf(1), qualityOf(1));
 
         new SellInUpdaterCommand().update(item);
 
@@ -23,7 +26,7 @@ public class SellInUpdaterCommandShould {
 
     @Test public void
     use_the_ConjuredSellInUpdater_for_a_Conjured_item_and_item_sellIn_should_decrease_by_1() {
-        Item item = createItemFrom(Constants.CONJURED, sellInOf(2), qualityOf(2));
+        Item item = createItemFrom(CONJURED, sellInOf(2), qualityOf(2));
 
         new SellInUpdaterCommand().update(item);
 
@@ -32,7 +35,7 @@ public class SellInUpdaterCommandShould {
 
     @Test public void
     use_the_StandardSellInUpdater_for_a_Standard_item_and_item_sellIn_should_decrease_by_1() {
-        Item item = createItemFrom(Constants.STANDARD_ITEM, sellInOf(3), qualityOf(5));
+        Item item = createItemFrom(STANDARD_ITEM, sellInOf(3), qualityOf(5));
 
         new SellInUpdaterCommand().update(item);
 
