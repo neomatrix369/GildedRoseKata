b0039569cdc118430c1ef909f9b15c28990b60e2
Mani Sarkar <sadhak001@gmail.com>
2015-10-13 18:13:26 +0100

    Added test for StandardSellInUpdaterShould

---
+++ src/test/java/com/gildedrose/StandardSellInUpdaterShould.java
@@ -0,0 +1,23 @@
+package com.gildedrose;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class StandardSellInUpdaterShould {
+    @Test public void
+    decrease_the_sell_in_of_an_item() {
+        int initialQuality = 10;
+        int initialSellIn = 6;
+        Item item = new Item(Constants.STANDARD_ITEM, initialSellIn, initialQuality);
+
+        StandardSellInUpdater standardSellInUpdater = new StandardSellInUpdater(item);
+        standardSellInUpdater.update();
+
+        int expectedSellIn = 5;
+        assertThat(item.sellIn, is(equalTo(expectedSellIn)));
+    }
+
+}
