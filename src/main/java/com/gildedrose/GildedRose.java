f21d24cd4a3388b23143cbc0f74bc0dba8ba13d3
Mani Sarkar <sadhak001@gmail.com>
2015-10-14 17:47:19 +0100

    Moved all item related methods to the Item class from the GildedRose class.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -5,13 +5,6 @@ class GildedRose {
     private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
     private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 
-    private static final int MINIMUM_QUALITY = 0;
-    private static final int MAXIMUM_QUALITY = 50;
-
-    private static final int ZERO_DAYS = 0;
-    private static final int SIX_DAYS = 6;
-    private static final int ELEVEN_DAYS = 11;
-
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -21,79 +14,15 @@ class GildedRose {
     public void updateQuality() {
         for (Item item : items) {
             if (AGED_BRIE.equals(item.name)) {
-                updateAgedBrie(item);
+                item.updateAgedBrie();
             } else if (BACKSTAGE_PASSES.equals(item.name)) {
-                updateBackstagePasses(item);
+                item.updateBackstagePasses();
             } else if (SULFURAS.equals(item.name)) {
-                updateSulfuras(item);
+                item.updateSulfuras();
             } else {
-                updateStandardItem(item);
+                item.updateStandardItem();
             }
         }
     }
 
-    private void updateAgedBrie(Item item) {
-        increaseQualityOf(item);
-
-        decreaseSellInOf(item);
-
-        if (isExpired(item)) {
-            increaseQualityOf(item);
-        }
-    }
-
-    private void updateBackstagePasses(Item item) {
-        increaseQualityOf(item);
-
-        if (item.sellIn < ELEVEN_DAYS) {
-            increaseQualityOf(item);
-        }
-
-        if (item.sellIn < SIX_DAYS) {
-            increaseQualityOf(item);
-        }
-
-        decreaseSellInOf(item);
-
-        if (isExpired(item)) {
-            setQualityToMinimum(item);
-        }
-    }
-
-    private void updateSulfuras(Item item) {
-    }
-
-    private void updateStandardItem(Item item) {
-        decreaseQualityOf(item);
-
-        decreaseSellInOf(item);
-
-        if (isExpired(item)) {
-            decreaseQualityOf(item);
-        }
-    }
-
-    private void decreaseQualityOf(Item item) {
-        if (item.quality > MINIMUM_QUALITY) {
-            item.quality = item.quality - 1;
-        }
-    }
-
-    private void increaseQualityOf(Item item) {
-        if (item.quality < MAXIMUM_QUALITY) {
-            item.quality = item.quality + 1;
-        }
-    }
-
-    private boolean isExpired(Item item) {
-        return item.sellIn < ZERO_DAYS;
-    }
-
-    private void decreaseSellInOf(Item item) {
-        item.sellIn = item.sellIn - 1;
-    }
-
-    private void setQualityToMinimum(Item item) {
-        item.quality = MINIMUM_QUALITY;
-    }
 }
--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -2,6 +2,12 @@ package com.gildedrose;
 
 public class Item {
 
+    private static final int MAXIMUM_QUALITY = 50;
+    private static final int MINIMUM_QUALITY = 0;
+    private static final int ZERO_DAYS = 0;
+    private static final int ELEVEN_DAYS = 11;
+    private static final int SIX_DAYS = 6;
+
     public String name;
 
     public int sellIn;
@@ -14,8 +20,73 @@ public class Item {
         this.quality = quality;
     }
 
-   @Override
-   public String toString() {
+    @Override
+    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
+    }
+
+    void setQualityToMinimum() {
+        quality = MINIMUM_QUALITY;
+    }
+
+    void decreaseSellIn() {
+        sellIn = sellIn - 1;
+    }
+
+    boolean isExpired() {
+        return sellIn < ZERO_DAYS;
+    }
+
+    void increaseQuality() {
+        if (quality < MAXIMUM_QUALITY) {
+            quality = quality + 1;
+        }
+    }
+
+    void decreaseQuality() {
+        if (quality > MINIMUM_QUALITY) {
+            quality = quality - 1;
+        }
+    }
+
+    void updateAgedBrie() {
+        increaseQuality();
+
+        decreaseSellIn();
+
+        if (isExpired()) {
+            increaseQuality();
+        }
+    }
+
+    void updateBackstagePasses() {
+        increaseQuality();
+
+        if (sellIn < ELEVEN_DAYS) {
+            increaseQuality();
+        }
+
+        if (sellIn < SIX_DAYS) {
+            increaseQuality();
+        }
+
+        decreaseSellIn();
+
+        if (isExpired()) {
+            setQualityToMinimum();
+        }
+    }
+
+    void updateSulfuras() {
+    }
+
+    void updateStandardItem() {
+        decreaseQuality();
+
+        decreaseSellIn();
+
+        if (isExpired()) {
+            decreaseQuality();
+        }
     }
 }
