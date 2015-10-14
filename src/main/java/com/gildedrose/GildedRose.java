cc18417eba2c2b3ec6a5bf74718c338831117230
Mani Sarkar <sadhak001@gmail.com>
2015-10-14 14:41:24 +0100

    Flipped the .equals to start from the constant string rather than the original item.name.equals(...)

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -13,12 +13,12 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (item.name.equals(AGED_BRIE)
-                    || item.name.equals(BACKSTAGE_PASSES)) {
+            if (AGED_BRIE.equals(item.name)
+                    || BACKSTAGE_PASSES.equals(item.name)) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
 
-                    if (item.name.equals(BACKSTAGE_PASSES)) {
+                    if (BACKSTAGE_PASSES.equals(item.name)) {
                         if (item.sellIn < 11) {
                             if (item.quality < 50) {
                                 item.quality = item.quality + 1;
@@ -34,7 +34,7 @@ class GildedRose {
                 }
             } else {
                 if (item.quality > 0) {
-                    if (item.name.equals(SULFURAS)) {
+                    if (SULFURAS.equals(item.name)) {
 
                     } else {
                         item.quality = item.quality - 1;
@@ -42,23 +42,23 @@ class GildedRose {
                 }
             }
 
-            if (item.name.equals(SULFURAS)) {
+            if (SULFURAS.equals(item.name)) {
 
             } else {
                 item.sellIn = item.sellIn - 1;
             }
 
             if (item.sellIn < 0) {
-                if (item.name.equals(AGED_BRIE)) {
+                if (AGED_BRIE.equals(item.name)) {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
                     }
                 } else {
-                    if (item.name.equals(BACKSTAGE_PASSES)) {
+                    if (BACKSTAGE_PASSES.equals(item.name)) {
                         item.quality = item.quality - item.quality;
                     } else {
                         if (item.quality > 0) {
-                            if (item.name.equals(SULFURAS)) {
+                            if (SULFURAS.equals(item.name)) {
                                 continue;
                             }
                             item.quality = item.quality - 1;
