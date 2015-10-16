a7da1cfbf2db3c021417379740c8c4f38faea5ca
Mani Sarkar <sadhak001@gmail.com>
2015-10-16 12:02:07 +0100

    Created tests for Days class, updated Days with equals and hashcode methods

--- src/main/java/com/gildedrose/Days.java
+++ src/main/java/com/gildedrose/Days.java
@@ -1,5 +1,8 @@
 package com.gildedrose;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 public class Days {
     private int value;
 
@@ -17,5 +20,25 @@ public class Days {
 
     public int asNumber() {
         return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Days days = (Days) o;
+
+        return new EqualsBuilder()
+                .append(value, days.value)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(value)
+                .toHashCode();
     }
 }
