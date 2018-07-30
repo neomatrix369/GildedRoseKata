7d7a0e86935d44d1259cfb24e3ecd20ef60c6e1a
neomatrix369 <sadhak001@gmail.com>
2018-07-30 18:55:28 +0100

    Extracting methods for Aged brie and Backstage pass

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -10,53 +10,59 @@ class GildedRose {
     public void updateQuality() {
         for (Item item : items) {
             if (item.name.equals("Aged Brie")) {
-                if (item.quality < 50) {
-                    item.increaseQuality();
-                }
-
-                item.decreaseSellIn();
-
-                if (item.sellIn < 0) {
-                    if (item.quality < 50) {
-                        item.increaseQuality();
-                    }
-                }
+                updateAgedBrie(item);
             } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality < 50) {
-                    item.increaseQuality();
-                }
-                if ((item.sellIn < 11) && (item.quality < 50)) {
-                    item.increaseQuality();
-                }
-
-                if ((item.sellIn < 6) && (item.quality < 50)) {
-                    item.increaseQuality();
-                }
-
-                item.decreaseSellIn();
-
-                if (item.sellIn < 0) {
-                    item.setQualityToZero();
-                }
+                updateBackstagePass(item);
             }
 
-            if (item.name.equals("Aged Brie")) {
-            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            } else {
-                if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                } else {
+            if (!item.name.equals("Aged Brie") &&
+                    !item.name.equals("Backstage passes to a TAFKAL80ETC concert") &&
+                    !item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                if (item.quality > 0) {
+                    item.decreaseQuality();
+                }
+
+                item.decreaseSellIn();
+
+                if (item.sellIn < 0) {
                     if (item.quality > 0) {
                         item.decreaseQuality();
                     }
-
-                    item.decreaseSellIn();
-
-                    if (item.sellIn < 0) {
-                        if (item.quality > 0) {
-                            item.decreaseQuality();
-                        }
-                    }
                 }
+            }
+        }
+    }
+
+    private void updateBackstagePass(Item item) {
+        if (item.quality < 50) {
+            item.increaseQuality();
+            
+            if ((item.sellIn < 11) && (item.quality < 50)) {
+                item.increaseQuality();
+            }
+
+            if ((item.sellIn < 6) && (item.quality < 50)) {
+                item.increaseQuality();
+            }
+        }
+
+        item.decreaseSellIn();
+
+        if (item.sellIn < 0) {
+            item.setQualityToZero();
+        }
+    }
+
+    private void updateAgedBrie(Item item) {
+        if (item.quality < 50) {
+            item.increaseQuality();
+        }
+
+        item.decreaseSellIn();
+
+        if (item.sellIn < 0) {
+            if (item.quality < 50) {
+                item.increaseQuality();
             }
         }
     }
